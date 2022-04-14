a44244ebc2f57f805c478eead17b394fc63d6203
pescapil <pescapil@uc.cl>
2022-04-14 19:33:03 -0400

    Update conf.py
    Readthedocs fine

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,13 @@
 # -- Project information -----------------------------------------------------
 
 project = 'ScattPINNs'
-copyright = '2022, Paul Escapil-Inchauspé'
-author = 'Paul Escapil-Inchauspé'
+author = 'pescap'
+
+
+# -- General configuration ---------------------------------------------------
+
+# The full version, including alpha/beta/rc tags
+release = "0.1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,16 +33,24 @@ author = 'Paul Escapil-Inchauspé'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
+
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = "sphinx"
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -46,7 +59,50 @@ exclude_patterns = []
 #
 html_theme = 'alabaster'
 
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_options = {"rightsidebar": "true", "relbarbgcolor": "black"}
+
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []  # ['_static']
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
+# sys.path.insert(0, os.path.abspath('../..'))
+
+htmlhelp_basename = "EasyHPCdoc"
+
+
+# -- Options for LaTeX output ------------------------------------------------
+
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    # 'papersize': 'letterpaper',
+    # The font size ('10pt', '11pt' or '12pt').
+    # 'pointsize': '10pt',
+    # Additional stuff for the LaTeX preamble.
+    # 'preamble': '',
+    # Latex figure (float) alignment
+    # 'figure_align': 'htbp',
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title,
+#  author, documentclass [howto, manual, or own class]).
+latex_documents = [
+    ("index", "ScattPINNs.tex", "ScattPINNs Documentation", "pescap", "manual")
+]
