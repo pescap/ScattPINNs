6c444784208e395945978af05ad9f7f2aaa39a17
Elias Hawas <101342356+Ehaw04@users.noreply.github.com>
2022-05-26 14:31:30 -0400

    Minor changes

--- examples/forward/Helmholtz_Neumann_2d_complex_eikx.py
+++ examples/forward/Helmholtz_Neumann_2d_complex_eikx.py
@@ -1,4 +1,4 @@
-"""Backend supported: tensorflow.compat.v1, tensorflow, pytorch"""
+"""Backend supported: tensorflow.compat.v1, tensorflow, pytorch."""
 import deepxde as dde
 import numpy as np
 
@@ -9,40 +9,26 @@ precision_test = 10
 hard_constraint = True
 weights = 100  # if hard_constraint == False
 epochs = 20000 #TODO, testing, 25000 for worflow testing
-parameters = [1e-3, 5, 50, "sin"]
-
-# learning rate
-# depth
-# width
-# activation function
-
-
+parameters = [1e-3, 5, 50, "sin"] #learning rate, depth, width, activation function
 
 learning_rate, num_dense_layers, num_dense_nodes, activation = parameters
 
-
 def pde(x, y):
-    yRe, yIm = y[:, 0:1], y[:, 1:2]
-    
+    yRe, yIm = y[:, 0:1], y[:, 1:2] 
     
     dyRe_xx = dde.grad.hessian(y, x, component=0, i=0, j=0)
     dyRe_yy = dde.grad.hessian(y, x, component=0, i=1, j=1)
     
     dyIm_xx = dde.grad.hessian(y, x, component=1, i=0, j=0)
     dyIm_yy = dde.grad.hessian(y, x, component=1, i=1, j=1)
-
-
     
     return [-dyRe_xx - dyRe_yy - k0 ** 2 * yRe,
             -dyIm_xx - dyIm_yy - k0 ** 2 * yIm]
 
-
-#ok
 def func(x):
     real = np.real(np.exp(1j * k0 * x[:, 0:1]))
     imag = np.imag(np.exp(1j * k0 * x[:, 0:1]))
     return np.hstack((real, imag))
-
 
 def boundary(_, on_boundary):
     return on_boundary
@@ -63,16 +49,13 @@ wave_len = 2*np.pi / k0  # wavelength
 dim_x = 2 * np.pi
 n_wave = 20
 
-# The mesh element size is h_elem
 h_elem = wave_len / n_wave
 
 nx = int(dim_x / h_elem)
 
-
 geom = dde.geometry.Rectangle([0, 0], [dim_x, dim_x])
 #k0 = 2 * np.pi * n
 #wave_len = 1 / n
-
 
 bcRe = dde.icbc.NeumannBC(geom,func1 , boundary, component=0)
 bcIm = dde.icbc.NeumannBC(geom,func0 , boundary, component=1)
@@ -99,7 +82,6 @@ model.compile(
     "adam", lr=learning_rate, metrics=["l2 relative error"], 
     loss_weights=loss_weights
 )
-
 
 losshistory, train_state = model.train(epochs=epochs)
 dde.saveplot(losshistory, train_state, issave=True, isplot=True)
